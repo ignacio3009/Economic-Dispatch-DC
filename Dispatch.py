36e0291f045faee1bba5733fceb24ab5469c8e13
Ignacio Ñancupil <ignacio3009@hotmail.com>
2019-05-18 02:01:30 -0400

    Update 1
    - Fixed radial network results
    - Fixed capacity lines problem: changing Cap value now you can really affect the flows by lines.

--- Dispatch.py
+++ Dispatch.py
@@ -1,63 +1,16 @@
+#======================================================
+# MAIN FUNCTIONS
+#======================================================
 import pandas as pd
 import numpy as np
-
-#=========================================
-# LOAD DATA 
-#=========================================
-
-
-dfLineas=pd.read_excel('Datos.xlsx','ParametrosLineas')
-lineas=dfLineas.values
-dfGeneradores=pd.read_excel('Datos.xlsx','ParametrosGeneradores')
-generadores=dfGeneradores.values
-dfCargas=pd.read_excel('Datos.xlsx','ParametrosCargas')
-cargas=dfCargas.values
-dfNodos=pd.read_excel('Datos.xlsx','ParametrosNodos')
-nodos=dfNodos.values
-
-
-from __future__ import print_function
-from ortools.linear_solver import pywraplp
-
-#Numero barras
-NB=nodos[0,0]
-#Numero generadores
-NG=generadores.shape[0]
-
-#En pu
-X=createMatrixX(lineas,NB)
-B=createMatrixB(X)
-
-
-#Pmax, Pmin
-Pmin,Pmax=_initPmaxmin(generadores)
-
-
-#Flujos por lineas
-Pf=np.zeros((NB,NB))
-
-#Vector de potencia demandada
-Pd=_fillPd(NB)
-
-#Vector de potencia generada
-Pg=np.zeros(NB)
-
-#Costos
-Alpha, Beta, Gamma=_initCosts(generadores)
-
-
-#=========================================
-# FUNCTIONS 
-#=========================================
-
 Sbase=100
-def createMatrixX(lineas,NB):
+def createMatrixX(lines,NB):
     X=np.zeros((NB,NB))
-    for i in range(len(lineas)):
-        nodo_i=int(lineas[i,0])-1
-        nodo_j=int(lineas[i,1])-1
-        X[nodo_i,nodo_j]=lineas[i,3]
-        X[nodo_j,nodo_i]=lineas[i,3]
+    for i in range(len(lines)):
+        nodo_i=int(lines[i,0])-1
+        nodo_j=int(lines[i,1])-1
+        X[nodo_i,nodo_j]=lines[i,3]
+        X[nodo_j,nodo_i]=lines[i,3]
     return X
 
 def createMatrixB(X):
@@ -67,7 +20,10 @@ def createMatrixB(X):
             if i==j:
                 B[i,i]=sum(1/k for k in X[i,:] if k!=0)
             else:
-                B[i,j]=-1/X[i,j]
+                if X[i,j]!=0:
+                    B[i,j]=-1/X[i,j]
+                else:
+                    B[i,j]=0
     return B
 
 #Calculate power injeted and consumed at each bus
@@ -79,41 +35,121 @@ def fillPower(Pg,Pd,NB):
         DeltaP[i]=Pg[i]-Pd[i]
     return DeltaP
 
-def _fillPd(NB):
+def fillPd(loads,NB):
     Pd=[0]*NB
     for i in range(NB):
-        ii=int(cargas[i,0])
-        Pd[ii-1]=cargas[i,1]/Sbase
+        ii=int(loads[i,0])
+        Pd[ii-1]=loads[i,1]/Sbase
     return Pd
 
     
-def _initCosts(generadores):
-    NG=generadores.shape[0]
+def initCosts(generators):
+    NG=generators.shape[0]
     Alpha = [0]*NG
     Beta = [0]*NG
     Gamma = [0]*NG
     for i in range(NG):
-        Alpha[i]=generadores[i,3]
-        Beta[i]=generadores[i,4]
-        Gamma[i]=generadores[i,5]
+        Alpha[i]=generators[i,3]
+        Beta[i]=generators[i,4]
+        Gamma[i]=generators[i,5]
     return Alpha, Beta, Gamma
     
-def _initPmaxmin(generadores):
-    NG=generadores.shape[0]
+def initPmaxmin(generators):
+    NG=generators.shape[0]
     Pmax=[0]*NG
     Pmin=[0]*NG
     for i in range(NG):
-        ii=int(generadores[i,0])
-        Pmin[ii-1]=generadores[i,6]/Sbase
-        Pmax[ii-1]=generadores[i,7]/Sbase
+        ii=int(generators[i,0])
+        Pmin[ii-1]=generators[i,6]/Sbase
+        Pmax[ii-1]=generators[i,7]/Sbase
     return Pmin,Pmax
 
+def cost_der(x):
+    g=np.zeros_like(x)
+    n=len(Beta)
+    for i in range(n):
+        g[i]=Beta[i]
+    g[n:]=0
+    return g
+
+def cost_hess(x):
+    h=np.zeros((len(x),len(x)))
+    return h
+
+def makeConstraint(NB,NL,NG,slack,file,B,X,Pd,Cap):
+    dflines=pd.read_excel(file,'ParametrosLineas')
+    lines=dflines.values
+    binf=np.zeros(NB+NL)
+    bsup=np.zeros(NB+NL)
+    A=np.zeros((NB+NL,NG+NB))
+    for i in range(NB):
+        A[i,i]=1
+        for j in range(NG,NG+NB):
+            A[i,j]=B[i,j-NG]
+    #Constraints of flows (angles)
+    bsup[0:NB]=np.copy(Pd)
+    binf[0:NB]=np.copy(Pd)
+    k=0
+    for line in lines:
+        i=int(line[0])-1
+        j=int(line[1])-1
+        A[NB+k,NB+i]=1
+        A[NB+k,NB+j]=-1
+        bsup[NB+k]=Cap*X[i,j]
+        binf[NB+k]=-Cap*X[i,j]
+        k=k+1
+    #Take out slack angle (because theta0 = 0)
+    aux=[i for i in range(NG+NB) if i!=(NG+slack)]
+    return A[:,aux],binf,bsup
+    
+def readsystem(file):
+    dflines=pd.read_excel(file,'ParametrosLineas')
+    lines=dflines.values
+    dfgenerators=pd.read_excel(file,'ParametrosGeneradores')
+    generators=dfgenerators.values
+    dfloads=pd.read_excel(file,'ParametrosCargas')
+    loads=dfloads.values
+    dfbus=pd.read_excel(file,'ParametrosNodos')
+    bus=dfbus.values
+    #Number of busbar
+    NB=bus[0,0]
+    #Number og generators
+    NG=generators.shape[0]
+    #Number of lines
+    NL=len(lines)
+    #En pu
+    X=createMatrixX(lines,NB)
+    B=createMatrixB(X)
+    #Pmax, Pmin
+    Pmin,Pmax=initPmaxmin(generators)
+    #Vector de potencia demandada
+    Pd=fillPd(loads,NB)
+    #Costos
+    Alpha, Beta, Gamma=initCosts(generators)
+    return NB,NL,NG,Pd, Alpha, Beta, Gamma,X,B,Pmin,Pmax
 
 
-#=========================================
-# STARTING ALGORITHM 
-#=========================================
 
+#======================================================
+# INIT VARIABLES
+#======================================================
+import pandas as pd
+import numpy as np
+from __future__ import print_function
+from ortools.linear_solver import pywraplp
+
+#Cargar datos
+file = 'Data.xlsx'
+NB,NL,NG,Pd, Alpha, Beta, Gamma,X,B,Pmin,Pmax = readsystem(file)
+
+
+
+
+
+
+#======================================================
+# START OPTIMIZATION VARIABLES
+#======================================================
 from scipy.optimize import Bounds,LinearConstraint, minimize
 
 #bounds = Bounds([Pmin[1],Pmin[2],-2*np.pi,-2*np.pi],[Pmax[1],Pmax[2],2*np.pi,2*np.pi])
@@ -121,41 +157,25 @@ from scipy.optimize import Bounds,LinearConstraint, minimize
 bounds = Bounds([Pmin[0],Pmin[1],Pmin[2],-1,-1],[Pmax[0],Pmax[1],Pmax[2],1,1])
 
 
-#linear_constraint=LinearConstraint([1,1,1,0,0],[sum(Pd)],[sum(Pd)])
+slack=0
+Cap=0.1
+#from Ax=b, A matrix
+A,binf,bsup=makeConstraint(NB,NL,NG,slack,file,B,X,Pd,Cap)
+linear_constraint = LinearConstraint(A,binf,bsup)         
 
-Alpha, Beta, Gamma=_initCosts(generadores)
 
 Bp=B[1:3,1:3]
 Binv=np.linalg.inv(Bp)
 
 def fo(y):
-    #Pgen- Pcons
-    DP = (y[0:3]-Pd)[1:]
-    y[3:]=np.dot(Binv,DP)
-    
-    Angles=np.copy(y[3:])
-    Angles=np.insert(Angles,0,0,axis=0)
-    
-    y[0:3]=np.dot(B,Angles)+Pd
-    
-    #return Pgen[0]*Beta[0] + Pgen[1]*Beta[1]  + Pgen[2]*Beta[2]
     return np.dot(y[0:3],Beta)
 
-Cap=0.1
-#from Ax=b, A matrix
-linear_constraint = LinearConstraint([[1,0,0,-1/X[0,1],-1/X[0,2]],
-                                      [0,1,0,1/X[1,0]+1/X[1,2],-1/X[1,2]],
-                                      [0,0,1,-1/X[2,1],1/X[2,0]+1/X[2,1]], 
-                                      [0,0,0,-1,0],
-                                      [0,0,0,0,-1],
-                                      [0,0,0,1,-1]],
-                                     [Pd[0],Pd[1],Pd[2], -Cap*X[0,1], -Cap*X[0,2], -Cap*X[1,2]],        #Lower bounds constraints (bmin)
-                                     [Pd[0],Pd[1],Pd[2], Cap*X[0,1], Cap*X[0,2], Cap*X[1,2]])           #Upper bounds constraints (bmax)
 
-y0 = np.array([10,10,10,0,0])
-res = minimize(fo, y0, method='trust-constr',jac=cost_der, hess=cost_hess, constraints=linear_constraint,
+y0 = np.array([0.3,0.2,0.1,0,0])
+res = minimize(fo, y0, method='trust-constr',jac=cost_der,
+               hess=cost_hess, constraints=linear_constraint,
+               options={'verbose': 1,'maxiter':1024}, bounds=bounds)
                #options={'maxiter':10000,'verbose': 1},bounds=bounds)
-               options={'verbose': 1,'xtol':1e-8,'disp':True}, bounds=bounds)
 
 
 #PRINT RESULTS
@@ -167,8 +187,8 @@ theta0=0
 theta1=res.x[3]
 theta2=res.x[4]
 P01=(theta0-theta1)/X[0,1]
-P02=(theta0-theta2)/X[0,2]
 P12=(theta1-theta2)/X[1,2]
+#P02=(theta0-theta2)/X[0,2]
 print('--------------------------')
 print('Detalle Generacion')
 print('PG1 = ',round(res.x[0],2))
@@ -177,7 +197,7 @@ print('PG3 = ',round(res.x[2],2))
 print('--------------------------')
 print('Flujos')
 print('P01 = ',round(P01,2))
-print('P02 = ',round(P02,2))
+#print('P02 = ',round(P02,2))
 print('P12 = ',round(P12,2))
 print('--------------------------')
 print('Angulos')
